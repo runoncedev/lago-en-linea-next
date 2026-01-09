d03965b243edf932948f5798c6587e0bc6924d15
runoncedev <camilorivera86@gmail.com>
2026-01-09 00:27:57 -0300

    add layout

---
+++ app/sensor/[id]/layout.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function SensorLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-gray-100 dark:bg-gray-800">
+      <nav className="border-slate-200 dark:border-slate-700 border-b  px-4 py-2">
+        <div className="max-w-lg mx-auto">
+          <Link href="/" className="text-slate-300">
+            &gt;
+            Lago en Línea
+          </Link>
+        </div>
+      </nav>
+      {children}
+    </div>
+  )
+}
